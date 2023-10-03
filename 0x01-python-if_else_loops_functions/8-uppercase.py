c70f8bd35ffab3c32656ee76fcc3ef937492ab4a
kiplimoboor <kiplimoarapboor@gmail.com>
2023-10-03 16:31:42 +0000

    check uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for char in (str):
+        upper = char
+        if (ord(char) >= 97):
+            upper = chr(ord(char) - 32)
+        print("{}".format(upper), end = "")
+    print("")
