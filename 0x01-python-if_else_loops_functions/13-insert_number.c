e8872bb08d43105f6e08f01f2d516d970a8a323c
kiplimoboor <kiplimoarapboor@gmail.com>
2023-10-04 09:30:49 +0000

    inserts a node into sorted linked list

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_node - inserts a node in a sorted list
+ *
+ * @head: the liked list
+ * @number: data of the node
+ *
+ * Return: new node, NULL if failed
+ */
+
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new = malloc(sizeof(listint_t));
+	listint_t *temp = *head;
+
+	if (!new)
+		return (NULL);
+
+	new->n = number;
+	new->next = NULL;
+
+	while (temp->next && temp->next->n < number)
+		temp = temp->next;
+	new->next = temp->next;
+	temp->next = new;
+
+	return (new);
+}
