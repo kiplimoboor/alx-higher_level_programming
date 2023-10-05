35a4a5a505df12c589277237029ad8e314f2be33
kiplimoboor <kiplimoarapboor@gmail.com>
2023-10-05 19:17:44 +0000

    bytecode

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-import magic_calculation_102
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
 
-
-def calculate(a, b):
-    if a < b:
-        add = magic_calculation_102.add
-        sub = magic_calculation_102.sub
+    if (a < b):
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
         return c
     else:
         return sub(a, b)
+
