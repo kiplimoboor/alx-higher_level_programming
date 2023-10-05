2e68ce650daf4aac929576e8da3ed58c0013e87c
kiplimoboor <kiplimoarapboor@gmail.com>
2023-10-05 18:57:45 +0000

    bytecode

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
 import magic_calculation_102
 
+
 def calculate(a, b):
-    """Calculates the result of a magic calculation.
-
-    Args:
-    a: The first operand.
-    b: The second operand.
-
-    Returns:
-    The result of the magic calculation.
-    """
-
     if a < b:
         add = magic_calculation_102.add
         sub = magic_calculation_102.sub
