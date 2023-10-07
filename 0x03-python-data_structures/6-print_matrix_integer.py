cb34dcf388d73633eda86e0aecb7ae56a5408211
kiplimoboor <kiplimoarapboor@gmail.com>
2023-10-07 07:56:11 +0000

    prints a matrix of integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix[0]) == 0 and len(matrix) == 1:
-        print("")
-
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            if j == (len(matrix[i]) - 1):
-                print("{:d}".format(matrix[i][j]), end="")
-            else:
-                print("{:d} ".format(matrix[i][j]), end="")
+    if matrix:
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if j == (len(matrix[i]) - 1):
+                    print("{:d}".format(matrix[i][j]), end="")
+                else:
+                    print("{:d} ".format(matrix[i][j]), end="")
+            print("")
+    else:
         print("")
