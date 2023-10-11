44d4ac3ebaded46271884d8a9e9fb0e7b2a22035
kiplimoboor <kiplimoarapboor@gmail.com>
2023-10-11 19:06:05 +0000

    converts a Roman numeral to an integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,13 +2,15 @@
 def roman_to_int(roman_string):
     if not roman_string:
         return 0
+
     roman_values = {
         'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
     }
-    roman_string = roman_string.upper()
+
     value = 0
     current_value = 0
     next_value = 0
+
     for i in range(len(roman_string)):
         if roman_string[i] not in list(roman_values):
             return 0
