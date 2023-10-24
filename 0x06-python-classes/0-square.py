b6490afd45c9fa2d2f9e336e41ee312d757cf766
kiplimoboor <kiplimoarapboor@gmail.com>
2023-10-24 15:18:51 +0000

    class defining a square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    class Square:
-        """ This class defines a suqare"""
-        pass
+class Square:
+    """ This class defines a suqare"""
+    pass
