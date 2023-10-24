25badc16e90cbdafe3522f162683e9fee165afb8
kiplimoboor <kiplimoarapboor@gmail.com>
2023-10-24 20:08:02 +0000

    defines and prints square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Containes class that defines a square"""
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Finds area"""
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """Prints the square"""
+        for i in range(self.size):
+            for j in range(self.size):
+                print("#", end="")
+            print("")
