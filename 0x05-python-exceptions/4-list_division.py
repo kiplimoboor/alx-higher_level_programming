3fee6b29ca6d0011c7c920bd505757bf28f9a44d
kiplimoboor <kiplimoarapboor@gmail.com>
2023-10-25 11:56:01 +0000

    divides list items

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,16 +1,15 @@
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range(list_length):
+        quotient = 0
         try:
             quotient = my_list_1[i] / my_list_2[i]
-            new_list.append(quotient)
         except TypeError:
             print("wrong type")
-            new_list.append(0)
         except ZeroDivisionError:
             print("division by 0")
-            new_list.append(0)
         except IndexError:
             print("out of range")
-            new_list.append(0)
+        finally:
+            new_list.append(quotient)
     return new_list
