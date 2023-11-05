0dfa05e96f54137fe77f0029c7cc44d0ee903b08
kiplimoboor <kiplimoarapboor@gmail.com>
2023-11-05 18:22:20 +0000

    matrix division

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -19,29 +19,19 @@ def matrix_divided(matrix, div):
         ZeroDivisionError: when div is 0
     """
 
-    is_matrix = all(isinstance(row, list) for row in matrix)
-    is_number = False
-    for row in matrix:
-        is_number = all((isinstance(element, int) or isinstance(
-            element, float)) for element in row)
-        if not is_number:
-            break
-
-    if not is_matrix or not is_number:
+    if not isinstance(matrix, list) or matrix == []\
+            or not all(isinstance(row, list) for row in matrix)\
+            or not all(isinstance(el, int) or isinstance(el, float)
+                       for row in matrix for el in row):
         raise TypeError(
             "matrix must be a matrix (list of lists) of integers/floats")
 
-    if type(div) not in [int, float]:
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    new_matrix = []
-    row_len = len(matrix[0])
-    for row in matrix:
-        if len(row) != row_len:
-            raise TypeError("Each row of the matrix must have the same size")
-        new_row = list(map(lambda a: round(a / div, 2), row))
-        new_matrix.append(new_row)
-
-    return new_matrix
+    return ([list(map(lambda el: round(el / div, 2), row)) for row in matrix])
