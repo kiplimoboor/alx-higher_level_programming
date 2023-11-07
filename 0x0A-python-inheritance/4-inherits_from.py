f6bc4238ad50352793d1bfc21b648b523a360c77
kiplimoboor <kiplimoarapboor@gmail.com>
2023-11-07 12:50:34 +0300

    checks for subclasses

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module that checks if an instance is a subclass"""
+
+
+def inherits_from(obj, a_class):
+    """Checks if an instance is a subclass
+
+    Args:
+        obj (any): instance to check
+        a_class (class): parent class to check against
+    """
+
+    return issubclass(type(obj),  a_class) and type(obj) is not a_class
