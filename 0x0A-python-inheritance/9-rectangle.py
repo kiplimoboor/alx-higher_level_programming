1dea2cdc99a2db0d8bc5babe77f2fbcfae9071e0
kiplimoboor <kiplimoarapboor@gmail.com>
2023-11-07 17:16:22 +0300

    basic Rectange class with area

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""Module with Basic Rectangle Class"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class that inherits from Geometry class
+
+    Args:
+        BaseGeometry (class): parent class
+    """
+
+    def __init__(self, width, height):
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
+
+    def __str__(self):
+        return (f"[Rectangle] {self.__width}/{self.__height}")
+
+    def area(self):
+        return self.__width * self.__height
