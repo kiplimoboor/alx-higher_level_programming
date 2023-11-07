bfb77b0c84f31aa37ccf64e2e14cf7ca2057d6c5
kiplimoboor <kiplimoarapboor@gmail.com>
2023-11-07 23:59:18 +0300

    adds all arguments to a Python list, and then save them to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,16 +4,16 @@ and then save them to a file
 """
 
 import sys
-import os.path as path
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 args = sys.argv
 
-if path.isfile(filename):
-    arguments = load_from_json_file("add_item.json").extend(args[1:])
-else:
-    arguments = args[1:]
+try:
+    arguments = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    arguments = []
 
+arguments.extend(args[1:])
 save_to_json_file(arguments, "add_item.json")
