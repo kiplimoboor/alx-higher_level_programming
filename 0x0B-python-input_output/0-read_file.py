f4f410061a83ae6756700beb07f1d1e0e1aab8b3
kiplimoboor <kiplimoarapboor@gmail.com>
2023-11-07 18:06:55 +0300

    reads file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module that reads a file"""
+
+
+def read_file(filename=""):
+    """Reads a file and prints ot to stdout
+
+    Args:
+        filename (str, optional): The name of the file. Defaults to "".
+    """
+
+    with open(filename) as f:
+        print(f.read())
