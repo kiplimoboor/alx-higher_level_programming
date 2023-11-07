2de9a895e901dc6004a2717ac13a175636ddc8ad
kiplimoboor <kiplimoarapboor@gmail.com>
2023-11-07 13:05:32 +0300

    basic geometrical Class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Module with a geometry class"""
+
+
+class BaseGeometry:
+    """Class containing basic geometey"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
