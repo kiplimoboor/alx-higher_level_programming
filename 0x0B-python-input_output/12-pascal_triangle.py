31297d40849ac42e402eb702aa19d7b70e10bed0
kiplimoboor <kiplimoarapboor@gmail.com>
2023-11-08 19:47:42 +0300

    function to return pascal triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -16,16 +16,9 @@ def pascal_triangle(n):
 
     for i in range(n - 1):
         prev_row = triangle[i]
-
         row = [1]
-        for j in range(i):
-            try:
-                row.append(prev_row[j] + prev_row[j + 1])
-            except IndexError:
-                row.append(prev_row[j] + prev_row[0])
-
+        for j in range(len(prev_row) - 1):
+            row.append(prev_row[j] + prev_row[j + 1])
         row.append(1)
-
         triangle.append(row)
-
     return triangle
