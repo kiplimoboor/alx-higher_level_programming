9ef21c7431b0f7bedd38ef1ebe4e0227f4adfcab
kiplimoboor <kiplimoarapboor@gmail.com>
2023-11-13 22:13:08 +0300

    add writing file to json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -43,13 +43,9 @@ class Base:
             list_objs (list): list of objects to be saved
         """
 
-        if list_objs is None:
-            json_string = "[]"
-
-        else:
-            list_dictionaries = [i.to_dictionary() for i in list_objs]
-            json_string = cls.to_json_string(list_dictionaries)
-            filename = f"{cls.__name__}" + ".json"
+        list_dictionaries = [i.to_dictionary() for i in list_objs]
+        json_string = Base.to_json_string(list_dictionaries)
+        filename = cls.__name__ + ".json"
 
         with open(filename, 'w') as file:
             file.write(json_string)
