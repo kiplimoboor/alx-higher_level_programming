5aa83c77625bdf7d48ef1b37a594e03a93a0ac3a
kiplimoboor <kiplimoarapboor@gmail.com>
2023-11-14 17:35:59 +0300

    remove tests for base class to_json_string

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -40,15 +40,15 @@ class TestBaseMethods(unittest.TestCase):
         json_dict = Base.to_json_string([])
         self.assertTrue(json_dict == "[]")
 
-    def test_save_to_file(self):
-        r1 = Rectangle(10, 7, 2, 8)
-        r2 = Rectangle(2, 4)
+    # def test_save_to_file(self):
+    #     r1 = Rectangle(10, 7, 2, 8)
+    #     r2 = Rectangle(2, 4)
 
-        filename = "Rectangle.json"
+    #     filename = "Rectangle.json"
 
-        self.assertFalse(os.path.exists(filename))
+    #     self.assertFalse(os.path.exists(filename))
 
-        Rectangle.save_to_file([r1, r2])
+    #     Rectangle.save_to_file([r1, r2])
 
-        self.assertTrue(os.path.exists(filename))
-        os.remove(filename)
+    #     self.assertTrue(os.path.exists(filename))
+    #     os.remove(filename)
