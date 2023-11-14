2d61859505b88a8b32c18876bfbe21c210c39338
kiplimoboor <kiplimoarapboor@gmail.com>
2023-11-14 16:18:33 +0300

    tests for base class init

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -6,18 +6,22 @@ class TestBaseInit(unittest.TestCase):
     def test_no_args(self):
         b1 = Base()
         b2 = Base()
-        b3 = Base()
-        self.assertEqual(b1.id, 1)
-        self.assertEqual(b2.id, 2)
-        self.assertEqual(b3.id, 3)
+        self.assertEqual(b2.id, b1.id + 1)
 
     def test_with_args(self):
-        b1 = Base(1)
-        b2 = Base(10)
-        b3 = Base(100)
-        self.assertEqual(b1.id, 1)
-        self.assertEqual(b2.id, 10)
-        self.assertEqual(b3.id, 100)
+        b1 = Base(10)
+        b2 = Base(100)
+        self.assertEqual(b1.id, 10)
+        self.assertEqual(b2.id, 100)
+
+    def test_with_none(self):
+        b1 = Base(None)
+        b2 = Base(None)
+        self.assertEqual(b2.id, b1.id + 1)
+
+    def test_with_other_type_arg(self):
+        self.assertEqual(Base("Hello").id, "Hello")
+        self.assertEqual(Base([1, 2, 3]).id, [1, 2, 3])
 
 
 if __name__ == "__main__":
