940d82ab8f939e67a9b716f6a18475e7d669a1b1
kiplimoboor <kiplimoarapboor@gmail.com>
2023-11-14 22:56:59 +0300

    tests for rectangle area

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -7,134 +7,133 @@ import unittest
 
 class TestRectangleInit(unittest.TestCase):
     def test_parent_class(self):
-        r1 = Rectangle(1, 1)
-
-        self.assertTrue(type(r1), Base)
+        self.assertTrue(type(Rectangle(1, 1)), Base)
 
     def test_id(self):
         r1 = Rectangle(1, 1)
         r2 = Rectangle(1, 1)
         self.assertEqual(r2.id, r1.id + 1)
 
-        r3 = Rectangle(1, 1, 1, 1, 5)
-        self.assertEqual(r3.id, 5)
+        self.assertEqual(Rectangle(1, 1, 1, 1, 5).id, 5)
 
     def test_width(self):
-        r1 = Rectangle(1, 2)
-        self.assertEqual(r1.width, 1)
+        self.assertEqual(Rectangle(1, 2).width, 1)
 
         with self.assertRaises(TypeError):
-            r2 = Rectangle("Hello", 2)
+            Rectangle("Hello", 2)
 
         with self.assertRaises(ValueError):
-            r2 = Rectangle(0, 2)
+            Rectangle(0, 2)
         with self.assertRaises(ValueError):
-            r2 = Rectangle(-1, 2)
+            Rectangle(-1, 2)
 
     def test_height(self):
-        r1 = Rectangle(3, 4)
-        self.assertEqual(r1.height, 4)
+        self.assertEqual(Rectangle(3, 4).height, 4)
 
         with self.assertRaises(TypeError):
-            r2 = Rectangle(3, "World")
+            Rectangle(3, "World")
 
         with self.assertRaises(ValueError):
-            r2 = Rectangle(3, 0)
+            Rectangle(3, 0)
         with self.assertRaises(ValueError):
-            r2 = Rectangle(3, -4)
+            Rectangle(3, -4)
 
     def test_x(self):
-        r1 = Rectangle(1, 2)
-        self.assertEqual(r1.x, 0)
+        self.assertEqual(Rectangle(1, 2).x, 0)
 
-        r2 = Rectangle(1, 2, 3, 4)
-        self.assertEqual(r2.x, 3)
+        self.assertEqual(Rectangle(1, 2, 3, 4).x, 3)
 
         with self.assertRaises(TypeError):
-            r3 = Rectangle(1, 2, "Hello", 1)
+            Rectangle(1, 2, "Hello", 1)
 
         with self.assertRaises(ValueError):
-            r3 = Rectangle(0, 2, -1, 2)
+            Rectangle(0, 2, -1, 2)
 
     def test_y(self):
-        r1 = Rectangle(3, 4)
-        self.assertEqual(r1.y, 0)
+        self.assertEqual(Rectangle(3, 4).y, 0)
 
-        r2 = Rectangle(1, 2, 3, 4)
-        self.assertEqual(r2.y, 4)
+        self.assertEqual(Rectangle(1, 2, 3, 4).y, 4)
 
         with self.assertRaises(TypeError):
-            r3 = Rectangle(1, 2, 3, "Hello")
+            Rectangle(1, 2, 3, "Hello")
 
         with self.assertRaises(ValueError):
-            r3 = Rectangle(1, 0, 1, -2)
+            Rectangle(1, 0, 1, -2)
 
 
-class TestRectangleMethods(unittest.TestCase):
+class TestRectangleArea(unittest.TestCase):
     def test_area(self):
-        r1 = Rectangle(3, 2)
-        self.assertEqual(r1.area(), 6)
+        self.assertEqual(Rectangle(10, 2).area(), 20)
 
-        r2 = Rectangle(1, 5, 1, 1, 1)
-        self.assertEqual(r2.area(), 5)
+    def test_large_area(self):
+        r = Rectangle(999999999999999, 999999999999999999, 0, 0, 1)
+        self.assertEqual(r.area(), 999999999999998999000000000000001)
 
-    def test_str(self):
-        r1 = Rectangle(3, 5, 0, 0, 1)
-        output = io.StringIO()
-        sys.stdout = output
-        print(r1)
-        self.assertEqual(output.getvalue(), "[Rectangle] (1) 0/0 - 3/5\n")
+    def test_changes_attr(self):
+        r = Rectangle(1, 1)
+        self.assertEqual(r.area(), 1)
 
-        r2 = Rectangle(4, 6, 2, 1, 12)
-        output = io.StringIO()
-        sys.stdout = output
-        print(r2)
-        self.assertEqual(output.getvalue(), "[Rectangle] (12) 2/1 - 4/6\n")
+        r.width = 5
+        r.height = 10
+        self.assertEqual(r.area(), 50)
 
-    def test_display(self):
-        r1 = Rectangle(1, 1)
-        output = io.StringIO()
-        sys.stdout = output
-        r1.display()
-        self.assertEqual(output.getvalue(), "#\n")
+    # def test_str(self):
+    #     r1 = Rectangle(3, 5, 0, 0, 1)
+    #     output = io.StringIO()
+    #     sys.stdout = output
+    #     print(r1)
+    #     self.assertEqual(output.getvalue(), "[Rectangle] (1) 0/0 - 3/5\n")
 
-        r2 = Rectangle(2, 2, 2, 2, 5)
-        output = io.StringIO()
-        sys.stdout = output
-        r2.display()
-        self.assertEqual(output.getvalue(), "\n\n  ##\n  ##\n")
+    #     r2 = Rectangle(4, 6, 2, 1, 12)
+    #     output = io.StringIO()
+    #     sys.stdout = output
+    #     print(r2)
+    #     self.assertEqual(output.getvalue(), "[Rectangle] (12) 2/1 - 4/6\n")
 
-    def test_update_with_args(self):
-        r1 = Rectangle(1, 10, 3, 5, 25)
-        output = io.StringIO()
-        sys.stdout = output
-        print(r1)
-        self.assertEqual(output.getvalue(), "[Rectangle] (25) 3/5 - 1/10\n")
+    # def test_display(self):
+    #     r1 = Rectangle(1, 1)
+    #     output = io.StringIO()
+    #     sys.stdout = output
+    #     r1.display()
+    #     self.assertEqual(output.getvalue(), "#\n")
 
-        r1.update(1, 3, 2, 4, 3)
-        output = io.StringIO()
-        sys.stdout = output
-        print(r1)
-        self.assertEqual(output.getvalue(), "[Rectangle] (1) 4/3 - 3/2\n")
+    #     r2 = Rectangle(2, 2, 2, 2, 5)
+    #     output = io.StringIO()
+    #     sys.stdout = output
+    #     r2.display()
+    #     self.assertEqual(output.getvalue(), "\n\n  ##\n  ##\n")
 
-    def test_update_with_kwargs(self):
-        r1 = Rectangle(1, 10, 3, 5, 25)
+    # def test_update_with_args(self):
+    #     r1 = Rectangle(1, 10, 3, 5, 25)
+    #     output = io.StringIO()
+    #     sys.stdout = output
+    #     print(r1)
+    #     self.assertEqual(output.getvalue(), "[Rectangle] (25) 3/5 - 1/10\n")
 
-        r1.update(height=5, x=10, width=15, y=21, id=3)
-        self.assertEqual(r1.id, 3)
-        self.assertEqual(r1.width, 15)
-        self.assertEqual(r1.height, 5)
-        self.assertEqual(r1.x, 10)
-        self.assertEqual(r1.y, 21)
+    #     r1.update(1, 3, 2, 4, 3)
+    #     output = io.StringIO()
+    #     sys.stdout = output
+    #     print(r1)
+    #     self.assertEqual(output.getvalue(), "[Rectangle] (1) 4/3 - 3/2\n")
 
-    def test_update_with_args_and_kwargs(self):
-        r1 = Rectangle(1, 10, 3, 5, 25)
-        args = 1, 3, 2, 4, 3
-        kwargs = {"height": 5, "x": 10, "width": 15, "y": 21, "id": 3}
-        r1.update(*args, **kwargs)
+    # def test_update_with_kwargs(self):
+    #     r1 = Rectangle(1, 10, 3, 5, 25)
 
-        self.assertEqual(r1.id, 1)
-        self.assertEqual(r1.width, 3)
-        self.assertEqual(r1.height, 2)
-        self.assertEqual(r1.x, 4)
-        self.assertEqual(r1.y, 3)
+    #     r1.update(height=5, x=10, width=15, y=21, id=3)
+    #     self.assertEqual(r1.id, 3)
+    #     self.assertEqual(r1.width, 15)
+    #     self.assertEqual(r1.height, 5)
+    #     self.assertEqual(r1.x, 10)
+    #     self.assertEqual(r1.y, 21)
+
+    # def test_update_with_args_and_kwargs(self):
+    #     r1 = Rectangle(1, 10, 3, 5, 25)
+    #     args = 1, 3, 2, 4, 3
+    #     kwargs = {"height": 5, "x": 10, "width": 15, "y": 21, "id": 3}
+    #     r1.update(*args, **kwargs)
+
+    #     self.assertEqual(r1.id, 1)
+    #     self.assertEqual(r1.width, 3)
+    #     self.assertEqual(r1.height, 2)
+    #     self.assertEqual(r1.x, 4)
+    #     self.assertEqual(r1.y, 3)
