fbbf313a9ad0d3662628203d0ad77a89fcfc4b20
kiplimoboor <kiplimoarapboor@gmail.com>
2023-11-14 14:58:24 +0300

    test for Base init

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -1,6 +1,5 @@
-from models.base import Base
-
 import unittest
+from models.base import Base
 
 
 def setUpModule():
@@ -11,13 +10,16 @@ def tearDownModule():
     print("teardown module")
 
 
-class TestBase(unittest.TestCase):
+class TestFib(unittest.TestCase):
+
     def setUp(self):
         print("setUp")
         self.b1 = Base()
         self.b2 = Base()
         self.b3 = Base(10)
         self.b4 = Base()
+        self.b5 = Base(100)
+        self.b6 = Base("String")
 
     def tearDown(self):
         print("tearDown")
@@ -25,6 +27,8 @@ class TestBase(unittest.TestCase):
         del self.b2
         del self.b3
         del self.b4
+        del self.b5
+        Base.reset()
 
     @classmethod
     def setUpClass(cls):
@@ -34,11 +38,31 @@ class TestBase(unittest.TestCase):
     def tearDownClass(cls):
         print("tearDownClass")
 
-    def test_init(self):
+    def test_no_args(self):
         self.assertEqual(self.b1.id, 1)
         self.assertEqual(self.b2.id, 2)
+        self.assertEqual(self.b4.id, 3)
+
+    def test_args(self):
+        self.assertEqual(self.b3.id, 10)
+        self.assertEqual(self.b5.id, 100)
+        self.assertEqual(self.b6.id, "String")
+        self.assertEqual(Base(2.4).id, 2.4)
+
+    def test_no_args_plus_args(self):
+        self.assertEqual(self.b1.id, 1)
         self.assertEqual(self.b3.id, 10)
         self.assertEqual(self.b4.id, 3)
+
+    def test_change_id(self):
+        self.b1.id = 10
+        self.assertEqual(self.b1.id, 10)
+        self.b2.id = "String"
+        self.assertEqual(self.b2.id, "String")
+
+    def test_private_instance_access(self):
+        with self.assertRaises(AttributeError):
+            instances = self.b1.__nb_instances
 
 
 if __name__ == "__main__":
