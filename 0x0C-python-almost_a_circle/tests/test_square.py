c69d08cc8cf845870ce1546a477bc187fc986287
kiplimoboor <kiplimoarapboor@gmail.com>
2023-11-15 14:42:24 +0300

    test square.__init__()

---
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -0,0 +1,23 @@
+from models.square import Square
+from models.rectangle import Rectangle
+from models.base import Base
+import sys
+import unittest
+from helper import print_io
+
+
+class TestRectangleInit(unittest.TestCase):
+    def test_parent_class(self):
+        self.assertTrue(type(Square(1, 1)), Rectangle)
+
+    def test_id(self):
+        s1 = Square(1)
+        s2 = Square(1)
+        self.assertEqual(s2.id, s1.id + 1)
+
+        self.assertEqual(Square(1, 1, 1, 5).id, 5)
+
+    def test_attr(self):
+        s = Square(1)
+        r = Rectangle(1, 1)
+        self.assertEqual(s.__dict__.keys(), r.__dict__.keys())
