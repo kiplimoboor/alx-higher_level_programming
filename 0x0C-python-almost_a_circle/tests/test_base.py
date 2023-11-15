8e030b85729f63d9f1868512868d07bded704ede
kiplimoboor <kiplimoarapboor@gmail.com>
2023-11-15 13:19:09 +0300

    new tests

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -17,39 +17,3 @@ class TestBaseInit(unittest.TestCase):
         b2 = Base(100)
         self.assertEqual(b1.id, 10)
         self.assertEqual(b2.id, 100)
-
-
-# class TestBaseMethods(unittest.TestCase):
-#     def test_to_json_string(self):
-#         r1 = Rectangle(10, 2, 4, 5, 2)
-#         s1 = Square(1, 2, 2, 3)
-
-#         r1_dict = r1.to_dictionary()
-#         self.assertTrue(type(r1_dict) is dict)
-#         s1_dict = s1.to_dictionary()
-#         self.assertTrue(type(s1_dict) is dict)
-
-#         json_dict = Base.to_json_string([r1_dict])
-#         self.assertTrue(type(json_dict) is str)
-
-#         json_dict = Base.to_json_string([s1_dict])
-#         self.assertTrue(type(json_dict) is str)
-
-#         json_dict = Base.to_json_string([r1_dict, s1_dict])
-#         self.assertTrue(type(json_dict) is str)
-
-#         json_dict = Base.to_json_string([])
-#         self.assertTrue(json_dict == "[]")
-
-#     def test_save_to_file(self):
-#         r1 = Rectangle(10, 7, 2, 8)
-#         r2 = Rectangle(2, 4)
-
-#         filename = "Rectangle.json"
-
-#         self.assertFalse(os.path.exists(filename))
-
-#         Rectangle.save_to_file([r1, r2])
-
-#         self.assertTrue(os.path.exists(filename))
-#         os.remove(filename)
