beaa59c95169f70a3660522115a82d757d22180c
kiplimoboor <kiplimoarapboor@gmail.com>
2023-11-15 16:56:01 +0300

    test representation

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -17,3 +17,16 @@ class TestBaseInit(unittest.TestCase):
         b2 = Base(100)
         self.assertEqual(b1.id, 10)
         self.assertEqual(b2.id, 100)
+
+
+class TestBaseRepresentations(unittest.TestCase):
+    def test_to_json_string(self):
+        r = Rectangle(10, 7, 2, 8)
+        dictionary = r.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        self.assertTrue(type(json_dictionary) == str)
+
+        s = Square(10, 2, 4)
+        dictionary = s.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        self.assertTrue(type(json_dictionary) == str)
