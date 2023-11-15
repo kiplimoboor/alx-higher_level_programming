f839d9071a075c605c1d267813792161bb47ed88
kiplimoboor <kiplimoarapboor@gmail.com>
2023-11-15 15:51:51 +0300

    test square init

--- 0x0C-python-almost_a_circle/tests/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -4,3 +4,15 @@ from models.base import Base
 import sys
 import unittest
 from helper import print_io
+
+
+class TestSquareInit(unittest.TestCase):
+    def test_init(self):
+        self.assertTrue(type(Square(1)), Rectangle)
+
+        self.assertEqual(Square(1, 2, 4, 5).id,  5)
+
+    def test_size(self):
+        s1 = Square(1)
+        self.assertEqual(s1.size, 1)
+        self.assertEqual(s1.width, s1.height)
