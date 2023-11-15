6fcd6c7d81eb55858a4bd5f50ee6b4e098255684
kiplimoboor <kiplimoarapboor@gmail.com>
2023-11-15 13:22:50 +0300

    test init base

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -12,8 +12,8 @@ class TestBaseInit(unittest.TestCase):
         b2 = Base()
         self.assertEqual(b2.id, b1.id + 1)
 
-    # def test_with_args(self):
-    #     b1 = Base(10)
-    #     b2 = Base(100)
-    #     self.assertEqual(b1.id, 10)
-    #     self.assertEqual(b2.id, 100)
+    def test_with_args(self):
+        b1 = Base(10)
+        b2 = Base(100)
+        self.assertEqual(b1.id, 10)
+        self.assertEqual(b2.id, 100)
