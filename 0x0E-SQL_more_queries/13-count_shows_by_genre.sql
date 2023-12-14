18241f2916d20d3e80ec6a90b939d8387c503549
kiplimoboor <kiplimoboor@pm.me>
2023-12-14 23:30:36 +0300

    only genres

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- lists all genres and displays the number of shows linked to each
-    SELECT tv_genres.name as genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
+    SELECT tv_genres.name AS 'genre', COUNT(tv_show_genres.genre_id) AS 'number_of_shows'
       FROM tv_genres
 RIGHT JOIN tv_show_genres
         ON tv_show_genres.genre_id = tv_genres.id
