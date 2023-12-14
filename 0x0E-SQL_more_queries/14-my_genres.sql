b1c93bd869427d91fa4ea2a77089f39b2e57625f
kiplimoboor <kiplimoboor@pm.me>
2023-12-14 23:50:41 +0300

    genres for Dexter

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -5,4 +5,5 @@ LEFT JOIN tv_show_genres
        ON tv_genres.id = tv_show_genres.genre_id
 LEFT JOIN tv_shows
        ON tv_shows.id = tv_show_genres.show_id
-    WHERE tv_show_genres.show_id = (SELECT id FROM tv_shows WHERE title = 'Dexter')
+    WHERE tv_shows.title = 'Dexter'
+ ORDER BY name
