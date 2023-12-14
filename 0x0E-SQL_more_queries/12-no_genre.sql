ac61c83058488937bb037d5ae6884eabfe2e6e6c
kiplimoboor <kiplimoboor@pm.me>
2023-12-14 16:36:44 +0300

    no genre

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,7 @@
---  lists all shows that have no genre
+--  lists all shows that have at least one genre linked
     SELECT tv_shows.title, tv_show_genres.genre_id
       FROM tv_shows
-RIGHT JOIN tv_show_genres
+ LEFT JOIN tv_show_genres
         ON tv_shows.id = tv_show_genres.show_id
+     WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id
