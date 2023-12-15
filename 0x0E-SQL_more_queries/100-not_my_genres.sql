d6892b33e43801d9a6f7bcdd8bf030719869c357
kiplimoboor <kiplimoboor@pm.me>
2023-12-15 23:09:28 +0300

    not dexter genre

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,15 @@
+-- Genres not Dexter
+
+SELECT name
+  FROM tv_genres
+ WHERE name NOT IN
+                    (
+                       SELECT name
+                         FROM tv_genres
+                    LEFT JOIN tv_show_genres
+                           ON tv_genres.id = tv_show_genres.genre_id
+                    LEFT JOIN tv_shows
+                           ON tv_shows.id = tv_show_genres.show_id
+                        WHERE tv_shows.title = 'Dexter'
+                    )
+ORDER BY name;
