12bca24abc41722bc279e4c5567a04cd199d542f
kiplimoboor <kiplimoboor@proton.me>
2024-01-09 13:37:20 +0300

    square extends a square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+class Square extends require('./5-square') {
+  charPrint (c = 'X') {
+    for (let i = 0; i < this.height; i++) {
+      console.log(`${c}`.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
