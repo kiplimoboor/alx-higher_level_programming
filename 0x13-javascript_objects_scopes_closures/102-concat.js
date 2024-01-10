d56ba34860dcd9cc0dc4603a65c2dc22ca57de77
kiplimoboor <kiplimoboor@proton.me>
2024-01-10 10:26:27 +0300

    concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,12 +1,20 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-fs.readFile(process.argv[2], (err, inputD) => {
-  if (err) throw err;
-  console.log(inputD.toString());
-});
 
-fs.readFile(process.argv[3], (err, inputD) => {
-  if (err) throw err;
-  console.log(inputD.toString());
-});
+const inputFiles = [process.argv[2], process.argv[3]];
+const outputFile = process.argv[4];
+
+for (let i = 0; i < inputFiles.length; i++) {
+  fs.readFile(inputFiles[i], 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    fs.appendFile(outputFile, data, err => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  });
+}
