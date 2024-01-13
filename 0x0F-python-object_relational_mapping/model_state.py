4f0dadea3853cc3ef533207d2b17506445501028
kiplimoboor <kiplimoarapboor@gmail.com>
2024-01-13 19:46:26 +0300

    SQLAlchemy: state class

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,14 +4,17 @@
     Creates a State Class
 """
 
-from sqlalchemy import create_engine, Column, Integer, String, MetaData
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
 
 
 mymetadata = MetaData()
 Base = declarative_base(metadata=mymetadata)
 
 class State(Base):
+    """
+        Class that maps to a table with same attributes 
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True,
                 nullable=False, unique=True)
