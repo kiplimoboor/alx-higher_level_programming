b5647aafb3fbf761bdf2b040b27cdbac19ab9b76
kiplimoboor <kiplimoarapboor@gmail.com>
2024-01-13 23:35:30 +0300

    SQLAlchemy: insert

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import Session
+from sqlalchemy import insert, create_engine
+
+if __name__ == '__main__':
+    args = sys.argv
+    engine = create_engine(
+        f'mysql+mysqldb://{args[1]}:{args[2]}@localhost:3306/{args[3]}')
+    conn = engine.connect()
+    session = Session(conn)
+
+    state = State(name='Louisiana')
+
+    session.add(state)
+    session.commit()
