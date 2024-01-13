6d919eda8dc8e8e3162e7a76ab54ea5628be7944
kiplimoboor <kiplimoarapboor@gmail.com>
2024-01-13 21:23:36 +0300

    SQLAlchemy: select

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+    Lists all state objects
+"""
+
+import sys
+from sqlalchemy import select, create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+
+if __name__ == '__main__':
+    args = sys.argv
+
+    engine = create_engine(
+        f'mysql+mysqldb://{args[1]}:{args[2]}@localhost:3306/{args[3]}')
+    conn = engine.connect()
+    session = Session(conn)
+
+    result = session.execute(select(State)).scalars()
+
+    for state in result:
+        print(f"{state.id}: {state.name}")
