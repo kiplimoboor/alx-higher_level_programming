329d17dddd7af991bb43213ec5f35e887b036b5c
kiplimoboor <kiplimoarapboor@gmail.com>
2024-01-13 13:37:04 +0300

    MySQLdb: row based on name

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', user=args[1],
                          passwd=args[2], db=args[3], port=3306)
     cur = db.cursor()
-    cur.execute('SELECT * FROM states WHERE name = "{}"'.format(args[4]))
+    cur.execute('SELECT * FROM states WHERE name LIKE BINARY "{}"'.format(args[4]))
     rows = cur.fetchall()
 
     for row in rows:
