3bd4755a1a3b426a239a98c7e08a98a8c5c096ef
kiplimoboor <kiplimoarapboor@gmail.com>
2024-01-13 19:49:04 +0300

    SQLAlchemy: state class

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -14,7 +14,7 @@ Base = declarative_base(metadata=mymetadata)
 
 class State(Base):
     """
-        Class that maps to a table with same attributes 
+        Class that maps to a table with same attributes
     """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True,
