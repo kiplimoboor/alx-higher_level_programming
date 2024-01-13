dd65c7edda5b94c1ef9b9c7f0266210fe5d42e3e
kiplimoboor <kiplimoarapboor@gmail.com>
2024-01-13 12:09:03 +0300

    MySQLdb: show states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
+
+""" This module simply connects to the hbtn_0e_0_usa
+    database, and gets data from the states table.
+
+    It uses the localhost
+
+    Usage: <program> user password database
+"""
+
 import MySQLdb
 import sys
 
-
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+if __name__ == '__main__':
+    args = sys.argv
+    db = MySQLdb.connect(host='localhost', user=args[1],
+                         passwd=args[2], db=args[3], port=3306)
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
+
     for row in rows:
         print(row)
+
     cur.close()
     db.close()
