6959ec95006610a9d5ec6e790bcf9710667c3c2f
kiplimoboor <kiplimoarapboor@gmail.com>
2024-01-15 07:32:36 +0300

    SQLAlchemy: where

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -7,7 +7,7 @@
 """
 
 from sys import argv
-from sqlalchemy import create_engine, select
+from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from relationship_state import State
 from relationship_city import City
@@ -17,9 +17,11 @@ if __name__ == '__main__':
         f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}')
     session = Session(engine.connect())
 
-    rows = session.execute(select(State).order_by('id')).scalars()
+    rows = session.query(State).all()
 
     for row in rows:
         print(f'{row.id}: {row.name}')
         for city in row.cities:
-            print(f'    {city.id}: {city.name}')
+            print(f'\t{city.id}: {city.name}')
+
+    session.close()
