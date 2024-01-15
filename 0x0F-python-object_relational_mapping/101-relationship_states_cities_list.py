b69628f7baff86bc60bd0d5fee4e7653e5f628be
kiplimoboor <kiplimoarapboor@gmail.com>
2024-01-15 19:38:03 +0300

    SQLAlchemy: enhanced select

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
-
 """
-    Usage: <program> user passwd database
-
-    Lists all states, and corresponding cities
+All states via SQLAlchemy
 """
-
 from sys import argv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from relationship_state import State
+from relationship_state import Base, State
 from relationship_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
 
-if __name__ == '__main__':
-    engine = create_engine(
-        f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}')
-    session = Session(engine.connect())
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
-    rows = session.query(State).all()
+    session = Session(engine)
 
-    for row in rows:
-        print(f'{row.id}: {row.name}')
+    data = session.query(State).order_by(State.id).all()
+
+    for row in data:
+        print("{}: {}".format(row.id, row.name))
         for city in row.cities:
-            print(f'    {city.id}: {city.name}')
+            print("    {}: {}".format(city.id, city.name))
 
+    session.commit()
     session.close()
