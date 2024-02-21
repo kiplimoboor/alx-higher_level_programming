85aa850a7594fc344a20a97a02787831bc07ffbc
kiplimoboor <kiplimoboor@hotmail.com>
2024-02-21 12:48:14 +0300

    request and write

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+/*
+ * Get contents of a web page
+ */
+
+const request = require('request');
+const url = process.argv[2];
+const fs = require('fs');
+const filename = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (error) {
+    return;
+  }
+  fs.writeFile(filename, body, err => {
+    if (err) {
+      console.error(err);
+    }
+  });
+});
