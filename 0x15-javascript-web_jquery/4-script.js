761f8040eea2120170db536ef6394927a63289af
kiplimoboor <kiplimoboor@hotmail.com>
2024-02-28 09:26:18 +0300

    toggle classes

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,9 +1,3 @@
 $('#toggle_header').on('click', () => {
-  if ($('header').hasClass('green')) {
-    $('header').removeClass('green');
-    $('header').addClass('red');
-  } else {
-    $('header').removeClass('red');
-    $('header').addClass('green');
-  }
+  $('header').toggleClass('green red');
 });
