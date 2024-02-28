110dcb0f7b6291c7bbb44a460a34d754e50920ae
kiplimoboor <kiplimoboor@hotmail.com>
2024-02-28 12:37:22 +0300

    simple get jquery

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,8 @@
+$(() => {
+  $.ajax({
+    url: 'https://swapi-api.alx-tools.com/api/people/5/?format=json',
+    success: function (data) {
+      $('#character').text(data.name);
+    }
+  });
+});
